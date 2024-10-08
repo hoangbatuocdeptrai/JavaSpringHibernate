f29fb4739b9d1b4263927bb2ea792691363bd7a8
hoangbatuocdeptrai <102572153+hoangbatuocdeptrai@users.noreply.github.com>
2024-10-08 23:43:32 +0700

    hoang_add_08102024

--- ProjectHibernate/src/main/java/com/bkap/impl/ProductDaoImpl.java
+++ ProjectHibernate/src/main/java/com/bkap/impl/ProductDaoImpl.java
@@ -1,9 +1,12 @@
 package com.bkap.impl;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.jboss.jandex.Main;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -12,10 +15,10 @@ import com.bkap.entity.Category;
 import com.bkap.entity.Product;
 
 @Repository
-public class ProductDaoImpl implements ProductDao{
+public class ProductDaoImpl implements ProductDao {
 	@Autowired
 	private SessionFactory sessionFactory;
-	
+
 	@Override
 	public List<Product> getAll() {
 		Session session = sessionFactory.openSession();
@@ -41,7 +44,7 @@ public class ProductDaoImpl implements ProductDao{
 		Session session = sessionFactory.openSession();
 		try {
 			session.beginTransaction();
-			List list = session.createQuery("from Product where Name like ?1").setParameter(1, "%"+name+"%").list();
+			List list = session.createQuery("from Product where Name like ?1").setParameter(1, "%" + name + "%").list();
 			session.getTransaction().commit();
 			session.close();
 			return list;
@@ -78,11 +81,11 @@ public class ProductDaoImpl implements ProductDao{
 	}
 
 	@Override
-	public boolean insert(Product c) {
+	public boolean insert(Product p) {
 		Session session = sessionFactory.openSession();
 		try {
 			session.beginTransaction();
-			session.save(c);
+			session.save(p);
 			session.getTransaction().commit();
 			return true;
 		} catch (Exception e) {
@@ -96,11 +99,11 @@ public class ProductDaoImpl implements ProductDao{
 	}
 
 	@Override
-	public boolean update(Product c) {
+	public boolean update(Product p) {
 		Session session = sessionFactory.openSession();
 		try {
 			session.beginTransaction();
-			session.update(c);
+			session.update(p);
 			session.getTransaction().commit();
 			return true;
 		} catch (Exception e) {
@@ -120,8 +123,8 @@ public class ProductDaoImpl implements ProductDao{
 			session.beginTransaction();
 			int i = session.createQuery("delete from Product WHERE Id = :id").setParameter("id", id).executeUpdate();
 			session.getTransaction().commit();
-			if(i>0)
-				 return true; 
+			if (i > 0)
+				return true;
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -132,4 +135,98 @@ public class ProductDaoImpl implements ProductDao{
 		return false;
 	}
 
+	@Override
+	public List<Product> getAll(int page, int size) {
+		Session session = sessionFactory.openSession();
+		try {
+			session.beginTransaction();
+			Query<Product> query = session.createQuery("from Product", Product.class);
+			query.setFirstResult((page - 1) * size);
+			query.setMaxResults(size);
+			List<Product> list = query.list();
+			session.getTransaction().commit();
+			return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		} finally {
+			session.close();
+		}
+		return Collections.emptyList();
+	}
+
+//	@Override
+	public long count() {
+		Session session = sessionFactory.openSession();
+		try {
+			session.beginTransaction();
+			long count = (long) session.createQuery("select count(*) from Product").uniqueResult();
+			session.getTransaction().commit();
+			return count;
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		} finally {
+			session.close();
+		}
+		return 0;
+	}
+
+	@Override
+	public List<Product> searchByName(String name, int page, int size) {
+		try (Session session = sessionFactory.openSession()) {
+			session.beginTransaction();
+			Query<Product> query = session.createQuery("from Product where Name like ?1", Product.class);
+			query.setParameter(1, "%" + name + "%");
+			query.setFirstResult((page - 1) * size);
+			query.setMaxResults(size);
+			List<Product> list = query.list();
+			session.getTransaction().commit();
+			return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		return Collections.emptyList();
+	}
+
+//	 @Override
+	public long countByName(String name) {
+		try (Session session = sessionFactory.openSession()) {
+			session.beginTransaction();
+			long count = (long) session.createQuery("select count(*) from Product where name like ?1")
+					.setParameter(1, "%" + name + "%").uniqueResult();
+			session.getTransaction().commit();
+			return count;
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		return 0;
+	}
+
+	@Override
+	public List<Product> getAllNewProduct() {
+		Session session = sessionFactory.openSession();
+		try {
+	        session.beginTransaction();
+	        List<Product> productList = session.createQuery("from Product order by id DESC", Product.class)
+	                                           .setMaxResults(4)
+	                                           .getResultList();
+	        
+	        session.getTransaction().commit();
+	        return productList;
+	    } catch (Exception e) {
+			session.getTransaction().rollback();
+	        e.printStackTrace();
+	        return null;
+	    } finally {
+	        if (session != null) {
+	            session.close();
+	        }
+	    }
+	
+	}
+	
+	
 }
